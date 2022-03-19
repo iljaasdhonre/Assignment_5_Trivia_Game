d629f0b7dbacac3e8359b1c31da3882e5226773c
Maghawry <mag.hauwry@gmail.com>
2022-03-19 16:58:27 +0100

    getInputUser fuction draft to get questions added

--- src/utils/questions-api.js
+++ src/utils/questions-api.js
@@ -1,6 +1,8 @@
 // Fetch all categories
+
 const apiUrl = "https://opentdb.com/api_category.php";
 let categories = [];
+let questions = [];
 
  export async function getCategories() {
    await fetch(apiUrl)
@@ -11,6 +13,28 @@ let categories = [];
     return categories;
 }
 
+export async function getInputUser(category="", difficulty="easy", amount="10") {
+  if(category = "Any Category"){
+  await fetch(`${apiUrl}?amount=${amount}&difficulty=${difficulty}`)
+   .then((response) => response.json())
+   .then((result) => {
+    questions = result;
+   });
+   return questions;
+  }else {
+    await fetch(`${apiUrl}?amount=${amount}&difficulty=${difficulty}&category=${category}`)
+   .then((response) => response.json())
+   .then((result) => {
+    questions = result;
+   });
+   return questions;
+
+  }
+}
+
 /**
  * End fetch categories
+ * https://opentdb.com/api.php?amount=10
+ * https://opentdb.com/api.php?amount=10&category=9&difficulty=medium
+ * 
  */
