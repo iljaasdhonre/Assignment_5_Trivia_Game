5093bfb29b93650b4636cd3beafd1b7363bcd16c
iljaasd <iljaasd@gmail.com>
2022-03-20 23:51:20 +0100

    FEAT: user fetch functions move to store as actions

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,14 +1,12 @@
 import { createStore } from "vuex";
 
+const apiKey = '9c4e656f9d584e5fa37a48b92e8b568f'
+const Base_URL = "https://api-noroff.herokuapp.com/trivia"
+
 const store = createStore({
     state: {
         questionsUrl: "",
-        user: {
-            username: "",
-            highScore: 0,
-            id: 0
-        },
-        categories: [],
+        user: [],
         questions: []
     },
     mutations:{
@@ -20,8 +18,48 @@ const store = createStore({
         }
     },
     actions:{
+        async checkUserName({commit, dispatch}, username){
+            return fetch(`${Base_URL}?username=${username}`)
+                .then(response => response.json())
+                .then(user => {
+                  if(user.length == 0){
+                   dispatch("register", user);
+                  }
+                  else{
+                    commit("setUser", user)
+                  }
+                })
+                .catch(error => {
+                  console.log(error.message)
+                })
+            }
 
-    }
+    },
+        async register({commit},username){
+            return fetch(`${Base_URL}`, {
+                method: 'POST',
+                headers: {
+                    'X-API-Key': apiKey,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    username: username, 
+                    highScore: 0 
+                })
+            })
+            .then(response => {
+                if (!response.ok) {
+                throw new Error('Could not create new user')
+                }
+                return response.json()
+            })
+            .then(newUser => {
+                commit("setUser", newUser);
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+        }
 })
 
 export default store;
